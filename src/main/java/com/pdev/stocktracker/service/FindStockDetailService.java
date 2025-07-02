cc086a30079d97e05ca89679ecccbf1fb9ae4927
Renan Lessa <renanlessa@gmail.com>
2025-07-02 16:40:03 -0300

    feat: update application configuration to use environment variables and add local configuration file

--- src/main/java/com/pdev/stocktracker/service/FindStockDetailService.java
+++ src/main/java/com/pdev/stocktracker/service/FindStockDetailService.java
@@ -3,6 +3,7 @@ package com.pdev.stocktracker.service;
 import com.pdev.stocktracker.client.BrapiClient;
 import com.pdev.stocktracker.client.response.BrapiStockDataResponse;
 import com.pdev.stocktracker.client.response.BrapiStockResponse;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,8 +25,18 @@ public class FindStockDetailService {
     @Cacheable(value = "acao", key="#stock")
     public Optional<BrapiStockDataResponse> getBrapiStockDetail(String stock) {
         log.info("Consultando informações da ação: {} na Brapi", stock);
-        BrapiStockResponse brapiStockResponse = brapiClient.getStock(stock, token);
-        log.info("Retorno Brapi da ação {}: {}", stock, brapiStockResponse.getResults().get(0));
-        return Optional.of(brapiStockResponse.getResults().get(0));
+        try {
+            BrapiStockResponse brapiStockResponse = brapiClient.getStock(stock, token);
+
+            log.info("Retorno Brapi da ação {}: {}", stock, brapiStockResponse.getResults().get(0));
+            return Optional.of(brapiStockResponse.getResults().get(0));
+
+        } catch (FeignException e) {
+            log.error("Erro ao consultar a ação {}: {}", stock, e.getMessage());
+            return Optional.empty();
+        } catch (Exception ex) {
+            log.error("Erro desconhecido ao consultar a ação: {} na Brapi. {}", stock, ex.getMessage());
+            return Optional.empty();
+        }
     }
 }
